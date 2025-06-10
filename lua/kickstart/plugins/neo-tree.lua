c11133a4ccc60a0e055867f646a2ec6048b279f8
zackary-clark <zackary.a.clark@gmail.com>
2025-06-10 12:36:37 -0400

    open in NeoTree rather than netrw

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -13,6 +13,18 @@ return {
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
+  init = function()
+    vim.api.nvim_create_autocmd('BufEnter', {
+      group = vim.api.nvim_create_augroup('NeoTreeInit', { clear = true }),
+      callback = function()
+        local f = vim.fn.expand '%:p'
+        if vim.fn.isdirectory(f) ~= 0 then
+          vim.cmd('Neotree current dir=' .. f)
+          vim.api.nvim_clear_autocmds { group = 'NeoTreeInit' }
+        end
+      end,
+    })
+  end,
   opts = {
     close_if_last_window = true,
     event_handlers = {
@@ -24,6 +36,7 @@ return {
       },
     },
     filesystem = {
+      hijack_netrw_behavior = 'open_current',
       follow_current_file = {
         enabled = true,
         leave_dirs_open = true,
@@ -49,3 +62,5 @@ return {
     },
   },
 }
+
+-- vim: ts=2 sts=2 sw=2 et
