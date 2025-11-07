abec6b644d80d969055e394cddc983257b785045
zackary-clark <zackary.a.clark@gmail.com>
2025-11-07 13:42:15 -0500

    fix telescope fuzzy_refine keybind

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -56,11 +56,11 @@ return {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = { ['<c-f>'] = require('telescope.actions').to_fuzzy_refine },
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
